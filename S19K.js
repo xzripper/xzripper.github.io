a3f975bab5a8734f2b3e9bf3608302c0d02becf0
Evan <94743980+xzripper@users.noreply.github.com>
2025-12-03 23:00:29 +0100

    Add files via upload

--- S19K.js
+++ S19K.js
@@ -1,10 +1,11 @@
 /**
  * https://github.com/xzripper/SINE19000/
- * SINE19000 - This tiny module keeps the device screen awake without requiring HTTPS or SSL certificates, making it ideal for local development.
- * It keeps the screen awake by playing an extremely low-frequency, low-resolution 10 KB video in the background.
- */
+ * SINE19000 - A tiny module to keep mobile screens awake without requiring HTTPS or SSL certificates, ideal for local development.
+ * Keeps the screen awake by playing a hidden 10kB video with extremely low frequencies and resolution.
+ * @returns Nothing.
+*/
 export function addS19K() {
-  const s19kElement = document.createElement("video");
+  const s19kElement = document.createElement("video", {id: "S19KElem"});
 
   s19kElement.src = "https://github.com/xzripper/SINE19000/raw/refs/heads/main/S19K/SINE19000.mp4"
   s19kElement.loop = true;
@@ -21,4 +22,37 @@ export function addS19K() {
   s19kElement.play().catch((error) => {
     console.error("S19K: Failed to play S19K.", error);
   });
+}
+
+/**
+ * Update S19K activity state. Prints error if S19K wasn't added on the page before.
+ * @param {boolean} active 
+ * @returns Nothing.
+ */
+export function setS19KActivity(active) {
+  const s19kElement = document.getElementById("S19KElem");
+
+  if (!s19kElement) {
+    console.error("S19K: Can't change activity of non-existing S19K element.");
+
+    return;
+  }
+
+  s19kElement.hidden = !active;
+}
+
+/**
+ * Remove S19K element. Prints error in the console if S19K wasn't added on the page before.
+ * @returns Nothing.
+ */
+export function removeS19K() {
+  const s19kElement = document.getElementById("S19KElem");
+
+  if (!s19kElement) {
+    console.error("S19K: Can't destroy non-existing S19K element.");
+
+    return;
+  }
+
+  s19kElement.remove();
 }
