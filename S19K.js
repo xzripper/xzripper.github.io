ea6c9f6ed923f7ead99e5653482c7f4e90490965
Evan <94743980+xzripper@users.noreply.github.com>
2025-12-03 22:35:00 +0100

    Add files via upload

--- S19K.js
+++ S19K.js
@@ -1,7 +1,7 @@
 /**
  * https://github.com/xzripper/SINE19000/
- * SINE19000 - A tiny module to keep mobile screens awake without requiring HTTPS or SSL certificates, ideal for local development.
- * Keeps the screen awake by playing a hidden 10kB video with extremely low frequencies and resolution.
+ * SINE19000 - This tiny module keeps the device screen awake without requiring HTTPS or SSL certificates, making it ideal for local development.
+ * It keeps the screen awake by playing an extremely low-frequency, low-resolution 10 KB video in the background.
  */
 export function addS19K() {
   const s19kElement = document.createElement("video");
